2f3d6e6efb0bd9edbd2b2a393025d36243c101c6
vincent <innocent0705@gmail.com>
2023-05-14 11:53:37 +0800

    update api url

--- client/src/api/axios.client.js
+++ client/src/api/axios.client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import queryString from 'query-string';
 
-const baseURL = 'http://127.0.0.1:5000/api/v1';
+const baseURL = 'https://chatgpt-ai-app-production.up.railway.app/api/v1';
 
 const axiosClient = axios.create({
     baseURL,
